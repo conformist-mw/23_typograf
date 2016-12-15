b4363d0c001824dc58a02fb3b0734c0fa1d10ded
conformist-mw <conformist.mw@gmail.com>
2016-12-15 17:54:06 +0200

    complete base func with ajax request

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
+
+@app.route('/typograf', methods=['POST'])
+def typograf():
+    text = request.form['text']
+    return text.upper()
+
 
 if __name__ == "__main__":
     app.run()
