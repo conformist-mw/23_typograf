e16dba111c8f95177b835ddb81f99c67f4301e89
conformist-mw <conformist.mw@gmail.com>
2016-12-27 23:34:51 +0200

    make list of tuples for regex to shorten the code

--- typo.py
+++ typo.py
@@ -1,49 +1,35 @@
 import re
 # Chars
-ndash = '–'
 nbsp = '\u00A0'
 
-
-def replace_quotes(text):
-    return re.sub(r'([\'"])(.*?)(\1)', r'«\2»', text)
-
-
-def replace_hyphen(text):
-    text = re.sub(r'\b(\w{3,5})\s?[-—]\s?(\w{2})\b', r'\1-\2', text)
-    text = re.sub(r'\s-\s', r' — ', text)
-    return text
-
-
-def handle_phones(text):
-    phone = re.compile(
-        r'[\+\(]?(\d)[-\s+\(]?(\d{2,3})[-\s+\)]?\s?'
-        r'(\d{2,3})[-\s]?(\d{1,3})[-\s]?(\d+)')
-    return phone.sub(r'\1(\2)\3{0}\4{0}\5'.format(ndash), text)
-
-
-def remove_extra_spaces(text):
-    text = re.sub(r'\r\n', r'\n', text)
-    text = re.sub(r'\n{2,}', r'\n', text)
-    text = re.sub(r'[ \t]{2,}', r' ', text)
-    return text
-
-
-def number_with_words(text):
-    text = re.sub(r'(\d+[.,]?\d+)\s?([€$\w]+)', r'\1{}\2'.format(nbsp), text)
-    text = re.sub(r'[^0-9]([а-яА-Я.]+)\s?(\d+)', r' \1{}\2'.format(nbsp), text)
-    return text
-
-
-def small_units_handle(text):
-    text = re.sub(r'\b([\w]{1,2}\s+)\b', r'\1{}'.format(nbsp), text)
-    return text
+patterns = [
+    # handle quotes
+    (r'([\'"])(.*?)(\1)', r'«\2»'),
+    # replace dash within short words with hyphen
+    (r'\b(\w{3,5})\s?[-—]\s?(\w{2})\b', r'\1-\2'),
+    # replace hyphen with dash
+    (r'\s-\s', r' — '),
+    # remove win-like newlines
+    (r'\r\n', r'\n'),
+    # replace extra newlines
+    (r'\n{2,}', r'\n'),
+    # remove tabs
+    (r'\t', r' '),
+    # remove extra spaces
+    (r'[ ]{2,}', r' '),
+    # words followed by numbers
+    (r'(\d+[.,]?\d+)\s?([€$\w]+)', r'\1{}\2'.format(nbsp)),
+    # handle phone numbers
+    (r'\b[\+\(]?(\d)[-\s+\(]?(\d{2,3})[-\s+\)]?\s?'
+        r'(\d{2,3})[-\s]?(\d{1,3})[-\s]?(\d+)\b', r'\1(\2)\3–\4–\5'),
+    # numbers followed by words
+    (r'[^0-9]([а-яА-Яa-zA-Z.]+)\s?(\d+)', r' \1{}\2'.format(nbsp)),
+    # short words followed by nbsp
+    (r'\b([\w]{1,2}\s+)\b', r'\1{}'.format(nbsp)),
+]
 
 
 def typo(text):
-    text = replace_hyphen(text)
-    text = replace_quotes(text)
-    text = handle_phones(text)
-    text = remove_extra_spaces(text)
-    text = number_with_words(text)
-    text = small_units_handle(text)
+    for pattern, replacement in patterns:
+        text = re.sub(pattern, replacement, text)
     return text
